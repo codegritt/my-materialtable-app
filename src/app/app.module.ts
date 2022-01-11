8320f6050b9a978897efea012b0c9e5222029a1f
Gokul <gokulchandan18@gmail.com>
2022-01-11 13:38:34 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,32 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import { LayoutModule } from './layout/layout.module';
-import { HeaderComponent } from './layout/header/header.component';
-import { FooterComponent } from './layout/footer/footer.component';
-import { SidebarComponent } from './layout/sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    SidebarComponent 
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSidenavModule,
-    LayoutModule
+    BrowserAnimationsModule
+   
   ],
-  exports:[
-    HeaderComponent,
-    FooterComponent,
-    SidebarComponent 
-
-
-  ],
+ 
 
   providers: [],
   bootstrap: [AppComponent]
