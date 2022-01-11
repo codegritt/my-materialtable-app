77311dcd1b09cfcf9f1d81deed1857fb8a6a6204
Gokul <gokulchandan18@gmail.com>
2022-01-11 14:09:53 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
 
+import { MaterialModule } from './material/material.module';
 
 
 
@@ -17,7 +17,7 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MaterialModule
    
   ],
  
